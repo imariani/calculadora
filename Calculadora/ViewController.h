26eb13ff82bc7094140ed55fe25c781e59dbea3d
ignacio mariani <imariani@arcba016271.local>
2017-06-22 10:06:38 -0300

    Initial Commit

---
+++ Calculadora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculadora
+//
+//  Created by ignacio mariani on 22/6/17.
+//  Copyright © 2017 ignacio mariani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculadora/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Calculadora
+//
+//  Created by ignacio mariani on 22/6/17.
+//  Copyright © 2017 ignacio mariani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
